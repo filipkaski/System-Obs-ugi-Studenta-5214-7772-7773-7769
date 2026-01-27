ef2c77786596d0529d62662bafdb55080d807753
MateuszKuc-ai <retro.mtq@gmail.com>
2026-01-27 07:38:41 +0100

    Update PrzedmiotyDaoImpl.java

--- src/main/java/pl/SOS/dao/impl/PrzedmiotyDaoImpl.java
+++ src/main/java/pl/SOS/dao/impl/PrzedmiotyDaoImpl.java
@@ -1,1 +1,41 @@
+package main.java.pl.SOS.dao.impl;
 
+import main.java.pl.SOS.dao.PrzedmiotyDao;
+import main.java.pl.SOS.model.Przedmioty;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+public class PrzedmiotyDaoImpl implements PrzedmiotyDao {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Override
+    public void dodaj(Przedmioty przedmioty) {
+        entityManager.persist(przedmioty);
+    }
+
+    @Override
+    public void edytuj(Przedmioty student) {
+        entityManager.merge(przedmioty);
+    }
+
+    @Override
+    public void usun(int id) {
+        Przedmioty student = pobierzPoId(id);
+        if (student != null) {
+            entityManager.remove(przedmioty);
+        }
+    }
+
+    @Override
+    public Przedmioty pobierzPoId(int id) {
+        return entityManager.find(Przedmioty.class, id);
+    }
+
+    @Override
+    public List<Przedmioty> pobierzWszystkich() {
+        return entityManager.createQuery("SELECT s FROM Przedmioty s", Przedmioty.class).getResultList();
+    }
+}
